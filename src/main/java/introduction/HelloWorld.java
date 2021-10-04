8b5024d187ff7ff62d7746d9443ab41670c71fba
Michal Musialowicz <micmus4@st.amu.edu.pl>
2021-10-04 18:17:18 +0200

    commit elo

--- src/main/java/introduction/HelloWorld.java
+++ src/main/java/introduction/HelloWorld.java
@@ -1,8 +1,13 @@
 package introduction;
 
+import org.apache.log4j.Logger;
+
 public class HelloWorld {
 
-    public static void main(String [ ] args) {
-        System.out.println("Hello! Witaj!");
-    }
-}
+    final static private Logger log = Logger.getLogger( "name" );
+
+    public static void main( String [ ] args )
+    {
+        System.out.println( "Hello! Witaj!" );
+        log.info( "Jan Pawel II ");
+    }}
